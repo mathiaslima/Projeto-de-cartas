8441c5a7291ad3c61ecf53ba05f3e46ec5c662f3
MATHIAS MORAIS LIMA <mathiaslima@A305-24L.imd.local>
2020-03-12 12:32:20 -0300

    update

--- cards.cpp
+++ cards.cpp
@@ -12,6 +12,9 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 
 struct Card {
@@ -27,6 +30,63 @@ struct Card {
     unsigned short r; // rank
 };
 
+typedef bool (*Predicado)(Card);
+
+void readFile(std::vector<Card> & cards){
+    short unsigned int num;
+    short unsigned int num2;
+    std::ifstream file("input.txt", std::ios::in); 
+
+    while(file >> num){
+        
+        switch (num)
+        {
+        case 0:
+            file >> num2;
+            cards.push_back({Card::suit::clubs, (num2 - 1)});
+            break;
+        case 1:
+            file >> num2;
+            cards.push_back({Card::suit::diamonds, (num2 - 1)});
+            break;
+        case 2:
+            file >> num2;
+            cards.push_back({Card::suit::hearts, (num2 - 1)});
+            break;
+        case 3:
+            file >> num2;
+            cards.push_back({Card::suit::spades, (num2 - 1)});
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    file.close();
+
+    
+}
+bool is_figure(Card e){
+    return (e.r >= 11);
+}
+bool is_red(Card e){
+    return (e. >= 11);
+}
+
+
+Card * find_if(std::vector<Card> cards, Predicado predicad){
+
+    for (Card card : cards)
+    {
+        if(predicad(card)){
+            return & card; 
+        }
+    }
+
+}
+
+
 std::ostream& operator<<( std::ostream& os, const Card & c )
 {
     std::string ranks_to_str[]{"as", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "joker"};
@@ -39,15 +99,17 @@ std::ostream& operator<<( std::ostream& os, const Card & c )
 
 int main(void)
 {
+    std::vector<Card> cards;
+
     Card hand[] { {Card::suit::clubs, 2},
                   {Card::suit::hearts, 8},
                   {Card::suit::spades, 11} };
 
 
     Card c1{Card::suit::spades, 11}, c2{Card::suit::diamonds, 10};
+    readFile(cards);
 
-
-    for ( const auto& e : hand )
+    for ( const auto& e : cards )
     {
         std::cout << e << ", ";
     }
